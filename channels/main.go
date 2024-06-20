248085def5def7987ad2e0d75f011c2fc8e0d976
fruityty <danuphat.lim@gmail.com>
2024-06-20 16:37:28 +0700

    update channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,21 +13,44 @@ func main() {
 		"http://stackoverflow.com",
 		"http://youtube.com",
 		"http://golang.org",
-		"http://fruitytyty.com",
+		// "http://fruitytyty.com",
 	}
 
+	// create string channel
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		// create child go routine
+		go checkLink(link, c)
+	}
+
+	// go main routine wait to recieve message from channel
+	// fmt.Println(<-c)
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	for l := range c {
+		go func(link string) {
+			// sleep go routine
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
+	// sleep go routine
+	// time.Sleep(5 * time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	// send message to channel
+	c <- link
 }
